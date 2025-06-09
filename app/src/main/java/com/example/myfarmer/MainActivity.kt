69d95ef8f337e5946ca339f84601ac7ce8acb2a4
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-06-09 11:48:33 +0200

    init

---
+++ app/src/main/java/com/example/myfarmer/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myfarmer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.myfarmer.shared.theme.MyFarmerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFarmerTheme {
+            }
+        }
+    }
+}
