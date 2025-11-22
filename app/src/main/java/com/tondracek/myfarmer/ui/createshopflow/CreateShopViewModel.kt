74666d44e430c3e7f76832b4dc63547a2b468761
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-11-22 18:48:44 +0100

    fixed CreateShopViewModel.kt

--- app/src/main/java/com/tondracek/myfarmer/ui/createshopflow/CreateShopViewModel.kt
+++ app/src/main/java/com/tondracek/myfarmer/ui/createshopflow/CreateShopViewModel.kt
@@ -1,22 +1,17 @@
 package com.tondracek.myfarmer.ui.createshopflow
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.tondracek.myfarmer.common.image.model.ImageResource
+import com.tondracek.myfarmer.core.usecaseresult.UCResult
 import com.tondracek.myfarmer.openinghours.domain.model.OpeningHours
 import com.tondracek.myfarmer.productmenu.domain.model.ProductMenu
 import com.tondracek.myfarmer.shop.domain.model.ShopInput
 import com.tondracek.myfarmer.shop.domain.usecase.CreateShopUC
 import com.tondracek.myfarmer.shopcategory.domain.model.ShopCategory
 import com.tondracek.myfarmer.shoplocation.domain.model.ShopLocation
-import com.tondracek.myfarmer.ui.createshopflow.CreateShopFlowStateMachine.next
-import com.tondracek.myfarmer.ui.createshopflow.CreateShopFlowStateMachine.previous
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -25,65 +20,80 @@ class CreateShopViewModel @Inject constructor(
     private val createShop: CreateShopUC
 ) : ViewModel() {
 
-    private val _step: MutableStateFlow<CreateShopFlowStep> =
-        MutableStateFlow(CreateShopFlowStateMachine.initialStep())
+    private val _state = MutableStateFlow<CreateShopState>(CreateShopState.Creating.initial())
 
-    private val _shopInput: MutableStateFlow<ShopInput> = MutableStateFlow(ShopInput())
+    val state: StateFlow<CreateShopState> = _state
 
-    val state: StateFlow<CreateShopState> = combine(
-        _step,
-        _shopInput
-    ) { step, shopInput ->
-        CreateShopState.Creating(
-            step = step,
-            shopInput = shopInput
-        )
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = CreateShopState.Loading
-    )
-
-    fun goToNextStep() = _step.update { currentStep ->
+    fun goToNextStep() = _state.updateCreating { currentStep ->
         currentStep.next()
     }
 
-    suspend fun finishCreation() {
-        createShop(_shopInput.value)
-    }
-
-    /* UPDATE METHODS */
-
-    fun goToPreviousStep() = _step.update { currentStep ->
+    fun goToPreviousStep() = _state.updateCreating { currentStep ->
         currentStep.previous()
     }
 
-    fun updateName(name: String) = _shopInput.update {
+    suspend fun finishCreation() = _state.updateCreatingSuspend {
+        when (val result = createShop(it.shopInput)) {
+            is UCResult.Success<Unit> -> CreateShopState.Finished
+            is UCResult.Failure -> CreateShopState.Error(result)
+        }
+    }
+
+    /* UPDATE METHODS */
+    fun updateName(name: String) = _state.updateShopInput {
         it.copy(name = name)
     }
 
-    fun updateDescription(description: String) = _shopInput.update {
+    fun updateDescription(description: String) = _state.updateShopInput {
         it.copy(description = description)
     }
 
-    fun updateCategories(categories: List<ShopCategory>) = _shopInput.update {
+    fun updateCategories(categories: List<ShopCategory>) = _state.updateShopInput {
         it.copy(categories = categories)
     }
 
-    fun updateImages(images: List<ImageResource>) = _shopInput.update {
-        // TODO: getting the diff between old and new images will be done in updateShopUC, with PhotoStorage
+    fun updateImages(images: List<ImageResource>) = _state.updateShopInput {
         it.copy(images = images)
     }
 
-    fun updateMenu(menu: ProductMenu) = _shopInput.update {
+    fun updateMenu(menu: ProductMenu) = _state.updateShopInput {
         it.copy(menu = menu)
     }
 
-    fun updateLocation(location: ShopLocation) = _shopInput.update {
+    fun updateLocation(location: ShopLocation) = _state.updateShopInput {
         it.copy(location = location)
     }
 
-    fun updateOpeningHours(openingHours: OpeningHours) = _shopInput.update {
+    fun updateOpeningHours(openingHours: OpeningHours) = _state.updateShopInput {
         it.copy(openingHours = openingHours)
+    }
+
+    /* PRIVATE HELPERS */
+
+    suspend fun MutableStateFlow<CreateShopState>.updateCreatingSuspend(
+        update: suspend (CreateShopState.Creating) -> CreateShopState
+    ) = this.update {
+        when (it) {
+            is CreateShopState.Creating -> update(it)
+            else -> it
+        }
+    }
+
+    fun MutableStateFlow<CreateShopState>.updateCreating(
+        update: (CreateShopState.Creating) -> CreateShopState
+    ) = this.update {
+        when (it) {
+            is CreateShopState.Creating -> update(it)
+            else -> it
+        }
+    }
+
+    fun MutableStateFlow<CreateShopState>.updateShopInput(
+        update: (ShopInput) -> ShopInput
+    ) = this.update {
+        when (it) {
+            is CreateShopState.Creating -> it.copy(shopInput = update(it.shopInput))
+            else -> it
+        }
     }
 }
