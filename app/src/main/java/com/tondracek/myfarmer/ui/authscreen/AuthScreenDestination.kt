9381635ab1fbdf5397248eb992c4f2755be718c0
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-11-26 14:42:08 +0100

    move AuthScreenViewModel.kt to separate file

--- app/src/main/java/com/tondracek/myfarmer/ui/authscreen/AuthScreenDestination.kt
+++ app/src/main/java/com/tondracek/myfarmer/ui/authscreen/AuthScreenDestination.kt
@@ -1,16 +1,10 @@
 package com.tondracek.myfarmer.ui.authscreen
 
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.tondracek.myfarmer.ui.core.navigation.AppNavigator
 import com.tondracek.myfarmer.ui.core.navigation.Route
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 fun NavGraphBuilder.authScreenDestination() {
     composable<Route.AuthScreenRoute> {
@@ -23,20 +17,4 @@ fun NavGraphBuilder.authScreenDestination() {
             onError = { message -> viewModel.showError(context, message) }
         )
     }
-}
-
-@HiltViewModel
-class AuthScreenViewModel @Inject constructor(
-    private val appNavigator: AppNavigator,
-) : ViewModel() {
-
-    fun navigateToMainShopsScreen() =
-        appNavigator.navigate(Route.MainShopsRoute)
-
-    fun showError(context: Context, message: String) =
-        Toast.makeText(
-            context,
-            message,
-            Toast.LENGTH_LONG
-        ).show()
 }
