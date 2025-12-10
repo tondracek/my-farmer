6bbeec7202265cabf86d7d9c8be901d1dc6e3803
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-12-10 18:17:42 +0100

    release: 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.tondracek.myfarmer"
         minSdk = 27
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
