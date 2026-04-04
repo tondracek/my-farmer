50afdace0498387882dffd2ede4e8180d5c6adf0
Tomáš Ondráček <t.ondracek187@gmail.com>
2026-04-04 17:51:58 +0200

    release: 1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.tondracek.myfarmer"
         minSdk = 27
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.4.4"
+        versionCode = 13
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
